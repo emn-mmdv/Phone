209b21bf6043bac7427c6c646a6c17de34f9cad4
Emin Mamedov <75082416+cytank@users.noreply.github.com>
2023-04-06 18:40:53 +0000

    a

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 stat = true
 
+// Power
+    function power(){
+        if(stat==true){
+            document.querySelector('.power-down').classList.add('active')
+            setTimeout(()=>{stat = false},500)
+        }else{
+            document.querySelector('.power-down').classList.remove('active')
+            setTimeout(()=>{stat = true},500)
+        }
 
-function power(){
-    if(stat==true){
-        document.querySelector('.power-down').classList.add('active')
-        setTimeout(()=>{stat = false},500)
-    }else{
-        document.querySelector('.power-down').classList.remove('active')
-        setTimeout(()=>{stat = true},500)
+
+    }
+
+    function lock(){
+        document.querySelector('.enterance').classList.toggle('active')
     }
 
 
-}
 
 const dat = new Date()
 document.querySelector('.watch').innerHTML = dat.getHours() + ":" + dat.getMinutes()

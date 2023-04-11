a74588aac346554a3a255c8d371759b31cfc30bf
Emin Mamedov <75082416+cytank@users.noreply.github.com>
2023-04-11 18:12:31 +0000

    New

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const dat = new Date()
-
 // Time
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const dat = new Date()
+let day = weekday[dat.getDay()];
+
     function time(x){
         if(x=="minute"){
             if(dat.getMinutes().toString().length!==2){
@@ -17,4 +19,37 @@ const dat = new Date()
         }
     }
 
+// Pin Code
+var code = ''
+const correct_code = '2023'
+
+    function add_number(x){
+        if(code.length !== 4){
+            document.querySelectorAll('.code span')[code.length].classList.add('active')
+            code += x.innerHTML
+            if(code.length == 4){
+                if(code == correct_code){
+                    setTimeout(()=>{code='';
+                    for(i=0;i<4;i++){
+                        document.querySelectorAll('.code span')[i].classList.remove('active')
+                        document.querySelector('.code').classList.remove('invaild')
+
+                        
+                    }},500)
+                }else{
+                    document.querySelector('.code').classList.add('invaild')
+                    setTimeout(()=>{code='';
+                    for(i=0;i<4;i++){
+                        document.querySelectorAll('.code span')[i].classList.remove('active')
+                        document.querySelector('.code').classList.remove('invaild')
+                    }},500)
+                }
+            }
+        }
+    }
+
+
+
+
 document.querySelector('.watch').innerHTML = time('hour') + ":" + time("minute")
+document.querySelector('.date h3').innerHTML = day + " " + dat.getDate()

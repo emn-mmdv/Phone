bdea652d05b2df9e8cae44e5f75432ec72fede0d
Emin Mamedov <emin451234@gmail.com>
2023-10-04 01:39:37 +0400

    V2.0
    A better update of phone, which new ui and a new basement for newest operation system

--- js/main.js
+++ js/main.js
@@ -1,58 +1,23 @@
-// Time
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const dat = new Date()
-let day = weekday[dat.getDay()];
-
-    function time(x){
-        if(x=="minute"){
-            if(dat.getMinutes().toString().length!==2){
-                return "0"+dat.getMinutes()
-            }else{
-                return dat.getMinutes()
-            }
-        }else if(x=="hour"){
-            if(dat.getHours().toString().length!==2){
-                return "0"+dat.getHours()
-            }else{
-                return dat.getHours()
-            }
-        }
+//? Button animation
+items('.buttons div').forEach(button => {
+    button.onclick = () => {
+        addClass(button, 'active')
+        setTimeout(() => removeClass(button, 'active'), 300)
     }
+})
 
-// Pin Code
-    var code = ''
-    const correct_code = '2023'
-
-    function add_number(x){
-        if(code.length !== 4){
-            document.querySelectorAll('.code span')[code.length].classList.add('active')
-            code += x.innerHTML
-            if(code.length == 4){
-                if(code == correct_code){
-                    setTimeout(()=>{
-                        code='';
-                        for(i=0;i<4;i++){
-                            document.querySelectorAll('.code span')[i].classList.remove('active')
-                            document.querySelector('.code').classList.remove('invaild')
-                        }
-                        document.querySelector('.screen-lock').classList.add('open')
-                        setTimeout(()=>{document.querySelector('.screen-lock').style.display = 'none'},500)
-                        document.querySelector('.home').classList.remove('deactive')
-                    },500)
-                }else{
-                    document.querySelector('.code').classList.add('invaild')
-                    setTimeout(()=>{code='';
-                    for(i=0;i<4;i++){
-                        document.querySelectorAll('.code span')[i].classList.remove('active')
-                        document.querySelector('.code').classList.remove('invaild')
-                    }},500)
-                }
-            }
-        }
+function power(){
+    if(stats.phone.status){
+        stats.phone.status = false;
+        removeClass('.screen', 'active');
+    } else{
+        stats.phone.status = true;
+        addClass('.screen', 'active');
     }
+}
 
-
-
-
-document.querySelector('.watch').innerHTML = time('hour') + ":" + time("minute")
-document.querySelector('.date h3').innerHTML = day + " " + dat.getDate()
+//? Keyboard Shortcuts
+document.body.onkeyup = (event) => {
+    //! Power Button | Press P
+    if(event.keyCode === 80){power();}
+}
